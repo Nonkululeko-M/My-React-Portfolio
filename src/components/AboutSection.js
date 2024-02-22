f837d98cc075250b8905332848ba174dc680fd8a
Nonkululeko-M <nonkululekonancy@gmail.com>
2024-02-22 14:10:44 +0200

    working on about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -10,12 +10,39 @@ const AboutSection = () => {
             <h2>About Me </h2>
           </header>
           <div className="content">
+          <div className="about_info grid"> {/* Container for information */}
+            {/* Box for experience */}
+            <div className="about_box">
+                {/* Icon for experience */}
+               
+                <i class="bx bx-award about_icon"></i>
+                {/* Title for experience */}
+                <h3 className="about_title">Experience</h3>
+                {/* Subtitle for experience */}
+                <span className="about_subtitle">-Experience 2+  years General Assistant</span>
+                <span className='abouttext'>-6 mnths Software Developer Intern</span>
+            </div>
+
+            {/* Box for completed projects */}
+            <div className="about_box">
+                {/* Icon for completed projects */}
+                <i class="bx bx-briefcase-alt about_icon"></i>
+                {/* Title for completed projects */}
+                <h3 className="about_title">Education</h3>
+                {/* Subtitle for completed projects */}
+                <span className="about_subtitle">National Diploma:
+Information Technology in Communication Networks </span>
+            </div>
+
+            {/* Box for support */}
+           
+            </div> 
             <p>
             "Hello! I'm Nonkululeko Mzalwana, a passionate and creative software developer on a mission to turn innovative ideas into reality. With a solid foundation in Java, JavaScript, Python, and more, I thrive in both front-end and back-end development. What sets me apart is not just my technical expertise, but my commitment to continuous learning and adaptability.
 
-I'm a recent graduate with a National Diploma in Information Technology, specializing in Communication Networks. My journey into software development started as a General Assistant, where I honed my customer service skills and transitioned into the dynamic world of coding through a Software Development Program with CapaCiti.
+I'm a recent graduate with a National Diploma in Information Technology. My journey into software development started as a General Assistant, where I honed my customer service skills and transitioned into the dynamic world of coding through a Software Development Program with CapaCiti.
 
-I bring a unique blend of technical proficiency and a knack for problem-solving. Whether it's crafting clean code, collaborating with cross-functional teams, or diving into the latest tech trends, I'm eager to contribute to innovative projects. Let's connect and explore how my skills and passion can add value to your team!"
+I bring a unique blend of technical proficiency and a knack for problem-solving. Let's connect and explore how my skills and passion can add value to your team!"
             </p>
           </div>
         </div>
