1692bb0cbf72abb17f22d4e69832c3912a352f5b
Nonkululeko-M <nonkululekonancy@gmail.com>
2024-03-20 10:51:24 +0200

    changed colors

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar'; // Corrected component name and path
-import Home from './components/Home'; // Assuming you have a Home component
+import Home from './components/home'; // Assuming you have a Home component
 import AboutSection from './components/AboutSection';
 import Skills from './components/skills';
-import Project from './components/Project';
-import Contact from './components/Contact';
+import Project from './components/project';
+import Contact from './components/contact';
 import Testiomonials from './components/Testimonials';
 import Footer from './components/Footer';
 import ToggleSwitch from './components/light';
